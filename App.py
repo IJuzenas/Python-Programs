e2338134600e56f8b55fbc85caa8120601938c05
IJuzenas <juzenasignas@gmail.com>
2023-06-05 01:25:15 +0300

    F String

--- App.py
+++ App.py
@@ -13,3 +13,9 @@ myAge = 20
 myName = 'My name is Ignas'
 myMessage = '%s I am %s years old'
 print (myMessage % (myName, myAge))
+
+#F String
+myAge = 20 + 5 
+myName = 'My name is Ignas'
+myMessage = f'{myName} I am {myAge} years old'
+print (myMessage)
