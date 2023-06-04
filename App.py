eac5a90c5c21c2772c704e7c1ff49ac3b385dcb4
IJuzenas <juzenasignas@gmail.com>
2023-06-05 01:18:53 +0300

    Learning about Strings

--- App.py
+++ App.py
@@ -1,1 +1,9 @@
+message1 = 'He said, "Aren\'t can\'t shouldn\'t wouldn\'t." '
+print(message1)
+
+#Multiline Strings
+message2 = '''He said, "Aren't can't shouldn't wouldn't." '''
+print(message2)
+
+#Multiplying Strings
 print('Python ' * 3)
