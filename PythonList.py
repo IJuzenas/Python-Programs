c757d3aad1f4f4e333d4fddd3d4cb44af8c0e64c
IJuzenas <juzenasignas@gmail.com>
2023-06-07 17:39:13 +0300

    Python lists

---
+++ PythonList.py
@@ -0,0 +1,7 @@
+primeNumbers = [2, 3, 5, 7, 11]
+print(type(primeNumbers))
+names = ['Ignas', 'Vladas', 'Kristupas']
+primeAndPeople = ['Ignas', 11, 3, 'Vladas']
+
+newList = primeNumbers + names
+print(newList)
