e8033c1f7f94953f424b174f8241048b14db59ce
IJuzenas <juzenasignas@gmail.com>
2023-06-08 01:51:03 +0300

    Conditional statments

---
+++ ConditionalStatements.py
@@ -0,0 +1,23 @@
+cash = 5
+if cash == 10:
+    print('Buy Chocolate')
+    print('Hello')
+    print('How are you?')
+else:
+    print('I am fine')
+    print('My name is Ignas')
+    print( 5*5)
+
+money = 20
+if money == 80:
+    print('I am rich')
+elif money == 80:
+    print('I am happy')
+elif money == 60:
+    print('You are nice!')  
+else:
+    print('I am poor')  
+    
+    
+if money == 100 or money == 80 or money == 80 or money == 60:
+    print ('I am rich')
