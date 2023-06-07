11b22b43a1b18b09d2442f34354ab045a96169aa
IJuzenas <juzenasignas@gmail.com>
2023-06-07 17:48:09 +0300

    Learning list methods

--- PythonList.py
+++ PythonList.py
@@ -5,3 +5,21 @@ primeAndPeople = ['Ignas', 11, 3, 'Vladas']
 
 newList = primeNumbers + names
 print(newList)
+
+primeNumbers = [2, 3, 5, 7, 11]
+print (primeNumbers[2])
+
+bestPrime = primeNumbers[4]
+print(bestPrime)
+
+#Add numbers to list
+primeNumbers.append(13)
+print(primeNumbers)
+
+#Remove number from the list
+primeNumbers.remove(5)
+print(primeNumbers)
+
+#Clear all from the list
+primeNumbers.clear()
+print(primeNumbers)
