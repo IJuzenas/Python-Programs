a90aed27c5c0446f908b5ff1ce590cf618cb6a8b
IJuzenas <juzenasignas@gmail.com>
2023-06-09 02:31:01 +0300

    Functions

---
+++ Functions.py
@@ -0,0 +1,12 @@
+txt = 'Welcome to Softlinks'
+txt.upper()
+
+#def = defining the function
+def get_milk():
+    print ('Open door')
+    print ('Walk to the store')
+    print ('Buy milk')
+    print ('Return home with milk.')
+    
+#Calling the function    
+get_milk()
