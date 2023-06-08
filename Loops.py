a6f203af1a88185e51d827eb348210907856e639
IJuzenas <juzenasignas@gmail.com>
2023-06-09 02:03:06 +0300

    Nested loops

--- Loops.py
+++ Loops.py
@@ -7,8 +7,15 @@ for x in names:
     print(f"Hello {x}")
     
 num = 5
-
+#while loop
 while num < 10:
     num = num +1
     print ('My endless loop')
+    
+#Nested loops (loop inside loop)
+items = [['red', 'pink'], ['berry', 'grapes'], ['cat', 'dog']]
+
+for item in items:
+    for x in item:
+        print(x)  
     
